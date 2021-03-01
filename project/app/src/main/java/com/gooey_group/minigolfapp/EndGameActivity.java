391d517d902630b7de4d4deb2de9122831997d65
ashdizon <ashmdizon@gmail.com>
2021-03-01 12:28:20 -0500

    added activity files

---
+++ project/app/src/main/java/com/gooey_group/minigolfapp/EndGameActivity.java
@@ -0,0 +1,14 @@
+package com.gooey_group.minigolfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
+}
