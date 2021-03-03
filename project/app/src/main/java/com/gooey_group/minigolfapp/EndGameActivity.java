d1521838734d1b27ddeaf8d41591081aec6f5231
ayalajt <ayalajt@vcu.edu>
2021-03-02 23:23:03 -0500

    Created skeletal code and UI of end game activity

--- project/app/src/main/java/com/gooey_group/minigolfapp/EndGameActivity.java
+++ project/app/src/main/java/com/gooey_group/minigolfapp/EndGameActivity.java
@@ -3,12 +3,29 @@ package com.gooey_group.minigolfapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class EndGameActivity extends AppCompatActivity {
+
+    int numberOfPlayers;
+    Button homepageButton;
+    String playerWinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+        homepageButton = findViewById(R.id.returnHP);
+        homepageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EndGameActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
