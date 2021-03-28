a6bfbbdb8bf23dbefc47401d261b47ac80ce2b81
Johnny <nguyenjl6@vcu.edu>
2021-03-27 23:26:16 -0400

    Updating select activity ui

--- project/app/src/main/java/com/gooey_group/minigolfapp/SelectActivity.java
+++ project/app/src/main/java/com/gooey_group/minigolfapp/SelectActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SelectActivity extends AppCompatActivity {
@@ -14,8 +15,8 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
 
-        ImageView addGameBtn = findViewById(R.id.add_game_btn);
-        addGameBtn.setOnClickListener(new View.OnClickListener() {
+        Button createGameBtn = findViewById(R.id.new_game_btn);
+        createGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SelectActivity.this, NewGameActivity.class);
