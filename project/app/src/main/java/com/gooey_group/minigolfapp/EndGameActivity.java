c2f3f28bd96e64fe104f4ccd03abb48bbbd369fd
ayalajt <ayalajt@vcu.edu>
2021-04-16 00:47:45 -0400

    Added basic scrollview to results table

--- project/app/src/main/java/com/gooey_group/minigolfapp/EndGameActivity.java
+++ project/app/src/main/java/com/gooey_group/minigolfapp/EndGameActivity.java
@@ -1,16 +1,18 @@
 package com.gooey_group.minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
+import android.graphics.Typeface;
 import java.util.Arrays;
 
 import java.io.Serializable;
@@ -29,12 +31,13 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
     final int MAX_SCORE = 999;
 
 
-    // TODO: improved dynamic table, table shadow, scrollable table
+    // TODO: improved dynamic table, table shadow, table doesn't scroll header
     // make headers consistent, main page button not centered, make buttons same sizes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
 
         // intent that receives an intent of type Game
         if (getIntent().getSerializableExtra("GameInfo") != null) {
@@ -44,9 +47,11 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
             totalPlayers = gameData.numPlayers;
             // sort players based on total score
             Arrays.sort(gameData.players);
+            TableLayout playersTable = (TableLayout) findViewById(R.id.table1);
+            ScrollView scrollTable = (ScrollView) findViewById(R.id.scrollTable);
             if (totalPlayers <= 0) {
-                TableLayout playersTable = (TableLayout) findViewById(R.id.table1);
                 playersTable.setVisibility(View.GONE);
+                scrollTable.setVisibility(View.GONE);
                 currentPlayerNameView = (TextView) findViewById(R.id.name1);
                 currentPlayerScoreView = (TextView) findViewById(R.id.score1);
                 currentPlayerNameView.setText("No One!");
@@ -54,8 +59,9 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
             }
             // check for certain cases so the app screen is formatted better
             else if (totalPlayers == 1) {
-                TableLayout playersTable = (TableLayout) findViewById(R.id.table1);
                 playersTable.setVisibility(View.GONE);
+                scrollTable.setVisibility(View.GONE);
+
                 currentPlayerInfo = gameData.players[0];
                 currentPlayerNameView = (TextView) findViewById(R.id.name1);
                 currentPlayerScoreView = (TextView) findViewById(R.id.score1);
@@ -71,7 +77,7 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
                 currentPlayerScoreView.setText("Score: " + Integer.toString(currentPlayerScore));
             }
             else {
-                TableLayout playersTable = (TableLayout) findViewById(R.id.table1);
+                Typeface typeface = ResourcesCompat.getFont(this, R.font.open_sans);
                 currentPlayerInfo = gameData.players[0];
                 currentPlayerNameView = (TextView) findViewById(R.id.name1);
                 currentPlayerName = currentPlayerInfo.name;
@@ -85,10 +91,30 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
                     currentPlayerScore = MAX_SCORE;
                 }
                 currentPlayerScoreView.setText("Score: " + Integer.toString(currentPlayerScore));
-
                 int playerPosition = 2;
                 String playerPositionAndName = "";
-                for(int i = 1; i < tableSize; i++) {
+                TableRow trOne = (TableRow) findViewById((R.id.row1));
+                currentPlayerInfo = gameData.players[1];
+                currentPlayerName = currentPlayerInfo.name;
+                if (currentPlayerName.length() > 12) {
+                    currentPlayerName = currentPlayerName.substring(0, 12) + ".";
+                }
+                playerPositionAndName = " " + Integer.toString(playerPosition) + ". " + currentPlayerName;
+                playerPosition++;
+                TextView playerTwoNameView = (TextView) findViewById(R.id.row1Name);
+                playerTwoNameView.setText((playerPositionAndName));
+                playerTwoNameView.setTextSize(26);
+                TextView playerTwoScoreView = (TextView) findViewById(R.id.row1Score);
+                currentPlayerScore = currentPlayerInfo.totalScore;
+                if (currentPlayerScore > MAX_SCORE) {
+                    currentPlayerScore = MAX_SCORE;
+                }
+                playerTwoScoreView.setText(Integer.toString(currentPlayerScore));
+                playerTwoScoreView.setTextSize(26);
+
+
+
+                for(int i = 2; i < tableSize; i++) {
                     TableRow tr =  new TableRow(this);
                     TextView playerNameText = new TextView(this);
                     playerNameText.setTextSize(26);
@@ -103,9 +129,13 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
                     playerNameText.setText(playerPositionAndName);
                     //playerNameText.setHeight(150);
                     playerNameText.setBackgroundResource(R.color.white);
+                    playerNameText.setTypeface(typeface);
+
                     tr.addView(playerNameText);
                     TextView playerScoreText = new TextView(this);
                     playerScoreText.setTextSize(26);
+                    playerScoreText.setTypeface(typeface);
+
                     currentPlayerScore = currentPlayerInfo.totalScore;
                     if (currentPlayerScore > MAX_SCORE) {
                         currentPlayerScore = MAX_SCORE;
@@ -140,13 +170,25 @@ public class EndGameActivity extends AppCompatActivity implements Serializable {
         playerOne.setName("Joe");
         Player playerTwo = new Player(9);
         playerTwo.totalScore = 6;
-        playerTwo.setName("John");
+        playerTwo.setName("Andy");
         Player playerThree = new Player(9);
         playerThree.totalScore = 7;
         playerThree.setName("Andy");
-        gameData = new Game(3, 9);
+        Player playerFour = new Player(9);
+        playerFour.totalScore = 13;
+        playerFour.setName("Tom");
+        Player playerFive = new Player(9);
+        playerFive.totalScore = 9;
+        playerFive.setName("Drew");
+        Player playerSix = new Player(9);
+        playerSix.totalScore = 12;
+        playerSix.setName("Bob");
+        gameData = new Game(2, 9);
         gameData.players[0] = playerOne;
         gameData.players[1] = playerTwo;
         gameData.players[2] = playerThree;
+        gameData.players[3] = playerFour;
+        gameData.players[4] = playerFive;
+        gameData.players[5] = playerSix;
     }
 }
