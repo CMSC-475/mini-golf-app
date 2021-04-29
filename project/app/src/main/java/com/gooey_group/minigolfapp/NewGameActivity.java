b1893d405c020ebfd86febbe7b46a5ba790bb81a
Hope <hopeztoulon@gmail.com>
2021-04-28 22:48:38 -0400

    Added Scroll view and Dynamic for Add players button

--- project/app/src/main/java/com/gooey_group/minigolfapp/NewGameActivity.java
+++ project/app/src/main/java/com/gooey_group/minigolfapp/NewGameActivity.java
@@ -4,33 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+
 public class NewGameActivity extends AppCompatActivity {
-    int numPlayers, numHoles = 9; //TODO: set this properly later
-    String boardName;
+    int numPlayers, numHoles; //TODO: set this properly later
     TextView display;
-    EditText playerNames;
+//    EditText playerNames;
+    LinearLayout linearLayout;
+//    ArrayList<String> names;
+//    ArrayList<LinearLayout> linList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+        linearLayout = findViewById(R.id.playerLayout);
 
 //        getSupportActionBar().hide();
 
+//        default to 2 players
+        numPlayers = 1;
 
-        numPlayers = 2;
         Button plus = (Button) findViewById(R.id.addbtn);
         Button minus = (Button) findViewById(R.id.minusbtn);
         display = (TextView) findViewById(R.id.playertxt);
-//        EditText name = (EditText) findViewById(R.id.ScoreboardEditTxt);
 
-//        boardName = name.getText().toString();
 
         display.setText(Integer.toString(numPlayers)); //u need to use integer.tostring for numbers
 
@@ -38,7 +45,6 @@ public class NewGameActivity extends AppCompatActivity {
         /*
          * NUMOFHOLES BUTTONS
          */
-        //todo: custom button
         Button nineBtn = findViewById(R.id.nineHoleBtn);
         nineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +70,7 @@ public class NewGameActivity extends AppCompatActivity {
             public void onClick(View view) {
                 numPlayers = numPlayers + 1;
                 display.setText(Integer.toString(numPlayers));
+                playerText();
             }
         });
 
@@ -74,6 +81,7 @@ public class NewGameActivity extends AppCompatActivity {
                     display.setText(Integer.toString(numPlayers));
                 }//end if
                 else{
+                    delText();
                     numPlayers = numPlayers - 1;
                     display.setText(Integer.toString(numPlayers));
                 }
@@ -81,6 +89,8 @@ public class NewGameActivity extends AppCompatActivity {
         });
 
         //create game object
+
+        playerText();
         Game dummyGame = new Game(numPlayers, numHoles);
 
 
@@ -90,14 +100,18 @@ public class NewGameActivity extends AppCompatActivity {
         createGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText player1 = (EditText) findViewById(R.id.player1);
-                String player1Name = player1.getText().toString();
+//                EditText player1 = (EditText) findViewById(R.id.player1);
+//                String player1Name = player1.getText().toString();
+//
+//                EditText player2 = (EditText) findViewById(R.id.player2);
+//                String player2Name = player2.getText().toString();
 
-                EditText player2 = (EditText) findViewById(R.id.player2);
-                String player2Name = player2.getText().toString();
 
-                dummyGame.players[0].setName(player1Name);
-                dummyGame.players[1].setName(player2Name);
+//                    for(int i = 0; i < numPlayers; i ++){
+//                        dummyGame.players[i].setName(names.get(i));
+//                    }//end for
+//                dummyGame.players[0].setName(player1Name);
+//                dummyGame.players[1].setName(player2Name);
                 Intent intent = new Intent(NewGameActivity.this, ScoreboardActivity.class);
                 intent.putExtra("createdGame", dummyGame);
                 startActivity(intent);
@@ -105,6 +119,36 @@ public class NewGameActivity extends AppCompatActivity {
         });
 
     }//end onCreate
+
+    public void delText() {
+    }//delText
+
+    public void playerText(){
+
+            LinearLayout play = new LinearLayout(this);
+            play.setOrientation(LinearLayout.VERTICAL);
+
+            linearLayout.addView(play);
+
+                TextView textView = new TextView(this);
+                textView.setText("Player " + String.valueOf(numPlayers));
+                textView.setTextSize(24);
+
+                EditText editText = new EditText(this);
+                editText.setHint("Player " + String.valueOf(numPlayers) + " Name");
+//                String playerName = editText.getText().toString();
+//                names.add(playerName);
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                );
+                textView.setLayoutParams(params);
+                play.addView(textView);
+                play.addView(editText);
+
+
+    }//end playertext
 
 
 
